bdc7b135ea583d317a29c1561ff71c9ec53e935d
nison79 <georgenison@gmail.com>
2021-04-20 19:40:41 +0300

    Survey Done with Dynamic Airtable

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -4,8 +4,85 @@ import styled from "styled-components"
 import base from "./Airtable"
 import { FaVoteYea } from "react-icons/fa"
 
+
+
 const Survey = () => {
-  return <h2>survey component</h2>
+
+  const [items,setItems] = useState([]);
+  const [loading,setLoading] =useState(true)
+
+  const getRecords = async () => {
+    //get the records from Airtable
+    const records = await base('Survey')
+    .select({})
+    .firstPage()
+    .catch(err => console.log(err))
+    //get the newItems from Airtable
+    const newItems = records.map((record) => {
+      const {id,fields} =record
+      return {id,fields}
+    })
+    
+    setItems(newItems);
+    setLoading(false);
+  }
+
+  const giveVote = async id => {
+    setLoading(true)
+    const tempItems = [...items].map((item) => {
+      if(item.id === id) {
+        let {id,fields} =item
+        fields = {...fields,votes:fields.votes +1}
+        return {id ,fields}
+      } else {
+        return item
+      }
+    })
+
+    const records = await base('Survey')
+    //update the records in AirTable
+    .update(tempItems)
+    .catch(err => console.log(err))
+    
+    const newItems = records.map(record => {
+      const { id, fields } =record
+      return { id, fields}
+    })
+    setItems(newItems)
+    setLoading(false)
+  }
+ 
+
+  useEffect(() => {
+    getRecords();
+  },[])
+
+
+  return <Wrapper className="section">
+      <div className="container">
+        <Title title="survey"/>
+        <h3>most importand room in the house ?</h3>
+        {loading ? <h3>loading...</h3> : <ul>
+          {items.map((item) => {
+
+            const {id,fields:{name,votes}} = item
+            
+            return <li key={id}>
+              <div className="key">
+                {name.toUpperCase().substring(0,2)}
+              </div>
+              <div>
+                <h4>{name}</h4>
+                <p>{votes} votes</p>
+              </div>
+              <button onClick={() => giveVote(id)}>
+                <FaVoteYea />
+              </button>
+            </li>
+          })}
+          </ul>}
+      </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
