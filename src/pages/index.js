610f7ea28d3e45f463f59df07749656c56d660ff
nison79 <georgenison@gmail.com>
2021-04-20 11:48:31 +0300

    Projects Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,12 @@ import SEO from "../components/seo"
 
 
 const HomePage = ({ data }) => {
-  console.log(data)
+  const {allAirtable:{nodes:projects}} = data
   return (
     <Layout>
       <Hero />
       <About />
+      <Projects projects={projects} title="latest projects"/>
     </Layout>
   )
 }
